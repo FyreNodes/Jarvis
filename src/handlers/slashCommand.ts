e1e51130546d1d9c4f55a89f12e27cc6b49a0c6a
TheFallenSpirit <thefallenspirit@outlook.com>
2022-05-09 16:03:23 -0700

    Really should have thought about this earlier

--- src/handlers/slashCommand.ts
+++ src/handlers/slashCommand.ts
@@ -5,7 +5,7 @@ import { readdirSync } from 'fs';
 export const slashCommandHandler = async (client: Client) => {
 	const path: string = `${__dirname}/../commands/slash`;
 	await readdirSync(path).forEach(async (dir: string) => {
-		const commands: string[] = readdirSync(`${path}/${dir}`).filter((file: string) => file.endsWith('.ts'));
+		const commands: string[] = readdirSync(`${path}/${dir}`).filter((file: string) => file.endsWith('.ts') || file.endsWith('.js'));
 		for (const cmd of commands) {
 			const command: SlashCommand = require(`${path}/${dir}/${cmd}`);
 			await client.slashCommands.set(command.info.name, command);
