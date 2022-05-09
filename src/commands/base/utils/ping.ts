9f3f7d254a160b10346e22d1909b885ef3eb59ac
TheFallenSpirit <thefallenspirit@outlook.com>
2022-05-08 20:59:11 -0700

    Some updates.

--- src/commands/base/utils/ping.ts
+++ src/commands/base/utils/ping.ts
@@ -1,7 +1,16 @@
 import { CommandInfo, CommandRun } from '@/Interfaces';
+import { MessageEmbed } from 'discord.js';
 
 export const run: CommandRun = (client, message, args) => {
-	message.channel.send({ content: `${client.ws.ping}` });
+	const appPing = Date.now() - message.createdTimestamp;
+	let embed = new MessageEmbed({
+		title: 'System Ping',
+		color: '#1AB6DC',
+		description: `⏳️ App: ${appPing}ms\n💓 WS: ${client.ws.ping}ms\n⚙ Gen: ${(appPing + client.ws.ping) / 2}ms`,
+		footer: {text: 'Jarvis • Utility', iconURL: client.user.avatarURL()},
+		timestamp: Date.now()
+	});
+	message.channel.send({ embeds: [embed] });
 };
 
 export const info: CommandInfo = {
--- src/commands/slash/utils/ping.ts
+++ src/commands/slash/utils/ping.ts
@@ -1,11 +1,20 @@
 import { SlashCommandInfo, SlashCommandRun } from '@/Interfaces';
+import { MessageEmbed } from 'discord.js';
 
 export const run: SlashCommandRun = async (client, interaction) => {
-	interaction.reply({ content: `${client.ws.ping}` });
+	const appPing = Date.now() - interaction.createdTimestamp;
+	let embed = new MessageEmbed({
+		title: 'System Ping',
+		color: '#1AB6DC',
+		description: `⏳️ App: ${appPing}ms\n💓 WS: ${client.ws.ping}ms\n⚙ Gen: ${(appPing + client.ws.ping) / 2}ms`,
+		footer: {text: 'Jarvis • Utility', iconURL: client.user.avatarURL()},
+		timestamp: Date.now()
+	});
+	interaction.reply({ embeds: [embed] });
 };
 
 export const info: SlashCommandInfo = {
 	name: 'ping',
-	description: 'Simple ping command',
+	description: 'Shows the bots current ping.',
 	category: 'utils'
 };
