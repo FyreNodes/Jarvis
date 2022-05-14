7ff91d6343b35b4482e59fd94a1f912273f42357
TheFallenSpirit <thefallenspirit@outlook.com>
2022-05-14 16:57:29 -0700

    Revert "Correct database uri"
    This reverts commit ccd38998625f8a4e8b382f54ffc32a98b24f3adf.

--- src/database/connect.ts
+++ src/database/connect.ts
@@ -1,6 +1,6 @@
 import { connect } from 'mongoose';
 import { grey, yellow, white } from 'chalk';
 
-connect(`${process.env.MONGO_URL}/bot`).then(() => console.log(`${grey.bold('[')}${yellow.bold('DATABASE')}${grey.bold(']')} ${white('Successfully connected to the database.')}`));
+connect(process.env.MONGO_URL).then(() => console.log(`${grey.bold('[')}${yellow.bold('DATABASE')}${grey.bold(']')} ${white('Successfully connected to the database.')}`));
 
 export default () => {};
